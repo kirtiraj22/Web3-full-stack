ff505166f04913dd3ddf5dddbd9949d23703a49f
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-15 12:14:50 +0530

    Create License.sol

---
+++ Solidity/License.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+//we are supposed to mention the SPDX license identifier (although it is not compulsory but some compilers will give warning if we don't mention it)
+//we can mention any license(MIT is the least restrictive so it is preferred)
