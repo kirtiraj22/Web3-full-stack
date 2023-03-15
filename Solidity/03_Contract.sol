1be4595a29173f7134728da63f6d23ac237174bd
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-15 12:31:54 +0530

    Create 03_Contract.sol

---
+++ Solidity/03_Contract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;  
+
+//defining a contract
+//contract is a keyword and it tells the compiler that the next piece of the code is going to define the contract of solidity
+//we can think of contracts as a class in any Object oriented programming language like java, c++, etc
+//we can give any name to our contract
+//it is preferred to name contracts using CapWords style for eg : SimpleStorage, Player, ContractName, etc
+//everything inside the curly braces is the content of the contract
+
+contract ContractName {
+    
+}
