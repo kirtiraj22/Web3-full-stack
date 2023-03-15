861e639b6dd174074b656187e44eeee992481fa5
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-15 12:11:42 +0530

    Create Versions.sol
    The file contains different methods to declare the version of solidity

---
+++ Solidity/Versions.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.7; //This defines version of solidity(0.8.19 is latest)
+//using ^ before the version means that this code will work on any version above 0.8.7
+//we can also use just 0.8.7
+//we can also use >=0.8.7 <0.9.0 this means all versions between 0.8.7(inclusive) amd 0.9.0(exclusive)
+
+//defining versions at top in solidity is necessary
