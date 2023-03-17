ab0624ac585fb27a7d56cfe0e7c51d735bf053ee
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-17 11:12:05 +0530

    Create 19_Value.sol

---
+++ Solidity/19_Value.sol
@@ -0,0 +1,6 @@
+//We can access the value attribute using one of the global keywords in solidity.
+//i.e If we want to access the amount or value somebody is sending, we can use : 
+
+msg.value;
+
+//we can use this in our function
