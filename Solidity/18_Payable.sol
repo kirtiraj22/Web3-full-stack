9f8e201d4cc08de2b96df3d4dc64c019f8aa64da
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-17 11:02:01 +0530

    Create 18_Payable.sol

---
+++ Solidity/18_Payable.sol
@@ -0,0 +1,9 @@
+//the functions can be made as a payable function by adding a payable keyword in it
+//Syntax : 
+function function_name() public payable {
+  //now this function will be considered as a payable function
+}
+
+
+//NOTE : smart contracts can hold funds just like our wallets. Since everytime we deploy the contract, it gets a address
+//the address is nearly same as the wallet address that we have
