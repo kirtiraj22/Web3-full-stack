0f76c57f45b4b13cfd9e434d8ae6296568a811ef
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-17 10:55:31 +0530

    Create 17_Transactions.sol

---
+++ Solidity/17_Transactions.sol
@@ -0,0 +1,8 @@
+//Every transaction that we send have the following fields : 
+//1) Nonce : Transaction count for the account
+//2) Gas Price : Price per unit of Gas(in wei)
+//3) Gas Limit : maximum gas that this transaction can use
+//4) To : address that the transaction is sent to
+//5) Value : Amount of wei to send
+//6) Data : What to send to the "To" address
+//7) v, r, s : components of transaction signature
