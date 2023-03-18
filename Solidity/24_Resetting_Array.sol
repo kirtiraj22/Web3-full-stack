985539b4053f261f91aa45220a7c1a15f94a5916
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-18 21:11:13 +0530

    Create 24_Resetting_Array.sol

---
+++ Solidity/24_Resetting_Array.sol
@@ -0,0 +1,11 @@
+//we can completely reset an array by using the following syntax : 
+
+arrayName = new address[](0);
+
+//for eg :
+funders = new address[](0);
+
+//the above line of code will reset the funders array from 0th index
+//if we have 
+funders = new address[](1);
+//the above line will reset the array starting from the index 1.
