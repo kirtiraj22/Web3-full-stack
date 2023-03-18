e938fb8efd06e0d4f0e9b498c7e4c4bb9acd3825
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-18 22:17:46 +0530

    Create 26_Constructor.sol

---
+++ Solidity/26_Constructor.sol
@@ -0,0 +1,11 @@
+//Constructor in solidity : 
+
+//many a times when we deploy the contract, we want the deployer to be the owner of that contract
+
+//basically constructor is a function that immediately gets called when the smart contract is deployed
+
+//SYNTAX : 
+constructor(){
+  //do something...
+}
+
