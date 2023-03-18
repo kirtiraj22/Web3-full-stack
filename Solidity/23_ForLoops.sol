5d22c2c67e7314ca5f0c973c359fe57c5ca5757d
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-18 20:57:21 +0530

    Create 23_ForLoops.sol

---
+++ Solidity/23_ForLoops.sol
@@ -0,0 +1,11 @@
+//for loop in similar to the for loop in javascript or c++ 
+//the syntax of for loop is :
+
+for(starting index , ending index , operations(increment or decrement));
+
+//here we use the for keyword
+
+//eg : 
+for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+  //do something...
+}
