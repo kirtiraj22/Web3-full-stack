90072b55f487ff751919d36cd468f0657e77530a
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-20 21:22:17 +0530

    Update 02_Import_Ether.js

--- Hardhat/Etherjs/02_Import_Ether.js
+++ Hardhat/Etherjs/02_Import_Ether.js
@@ -5,3 +5,7 @@ const { ethers } = require("ethers");
 
 //for ES6 or TypeScript : 
 import { ethers } from "ethers";
+
+
+//this should be done globally i.e outside the main function
+//because we want to import all the packages before we call the main function
