cfff99785dc3505cd784606d3247ce75e63cd723
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-20 21:19:42 +0530

    Create Import_Ether.js

---
+++ Hardhat/Etherjs/Import_Ether.js
@@ -0,0 +1,7 @@
+//to import Etherjs library in our file we can do the following :
+
+//for Node.js :
+const { ethers } = require("ethers");
+
+//for ES6 or TypeScript : 
+import { ethers } from "ethers";
