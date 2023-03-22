dc41947883c074d20dfa80f6bc7859a21b0316d0
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2023-03-22 22:54:45 +0530

    Update 09_Interact_with_smartContracts.js

--- Hardhat/Etherjs/09_Interact_with_smartContracts.js
+++ Hardhat/Etherjs/09_Interact_with_smartContracts.js
@@ -15,5 +15,19 @@ const favoriteNumber = await contract.retrieve();
 //that retrieve function will return a uint which will be stored in our favoriteNumber variable
 
 
-//NOTE : we can convert it to string by using :
-console.log(favoriteNumber.toString());
+
+
+//suppose we want to use another function named as store through which we can update our favorite number
+// this can be done in a similar way :
+
+const currentFavoriteNumber = await contract.retrieve();
+console.log(`Current favorite number : ${currentFavoriteNumber.toString()}`); //we use toString() to convert the number to string
+//calling the store function(of smart contracts) and storing 12 in it :
+const transactionResponse = await contract.store(12);
+const transactionReceipt = await transactionResponse.wait(1);
+const updatedFavoriteNumber = await contract.retrieve();
+console.log(`Updated favorite Number is : ${updatedFavoriteNumber}`);
+
+//OUTPUT :
+Current favorite number : 0
+Updated favorite Number is : 12
