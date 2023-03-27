da27f2848666e706b07543a02a930d04b9a82554
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-03-27 15:21:54 +0530

    added gas reporter

--- Hardhat/hardhat-simple-storage/hardhat.config.js
+++ Hardhat/hardhat-simple-storage/hardhat.config.js
@@ -2,10 +2,13 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API = process.env.COINMARKETCAP_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
@@ -25,5 +28,13 @@ module.exports = {
   solidity: "0.8.8",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-reporter.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API,
+    // token: "MATIC",
   },
 };
